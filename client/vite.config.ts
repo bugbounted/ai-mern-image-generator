f520e606c6e11b9337dca6c404d7b1499b4f7536
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-23 23:59:12 +0330

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': 'https://ai-mern-image-generator-api.onrender.com',
+      '/api/v1': 'https://midjourneycore.onrender.com',
     },
   },
   plugins: [react()],
